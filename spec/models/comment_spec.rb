0e55bc51426bbaa2642fbf4919e84625e4782702
Kyle Henson <khenson.kh@gmail.com>
2015-08-21 14:05:00 -0600

    Added test to check that the markdown from RedCarpet is working on the Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -22,4 +22,10 @@ RSpec.describe Comment, type: :model do
     expect(comment2).not_to be_valid
     expect(comment3).to be_valid
   end
+
+  it 'creates a json object with markdown when markdownify is called' do
+    comment = Comment.create!(author: "jack", body: "<h2>Heading 2</h2><p><em>emphasis</em><br><strong>strong</strong></p>")
+
+    expect(comment.markdownify(comment.body)).to eq "{\"author\":\"jack\",\"body\":\"\\u003ch2\\u003eHeading 2\\u003c/h2\\u003e\\u003cp\\u003e\\u003cem\\u003eemphasis\\u003c/em\\u003e\\u003cbr\\u003e\\u003cstrong\\u003estrong\\u003c/strong\\u003e\\u003c/p\\u003e\"}"
+  end
 end
